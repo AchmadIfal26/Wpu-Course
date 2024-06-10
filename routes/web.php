fed677ce5aecca8ce12187b820d57b86af85a200
AchmadIfal26 <147014127+AchmadIfal26@users.noreply.github.com>
2024-06-10 14:01:07 +0700

    Layout Model

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+            'title' => 'Homepage'
+        ]
+    );
 });
 
 Route::get('/about', function () {
     return view('about', [
-        'nama' => 'Achmad Ifal',
+        'name' => 'Achmad Ifal',
+        'title' => 'About'
     ]);
 });
 
 Route::get('/contact', function () {
     return view('contact', [
         'email' => 'qwerty@gmail.com',
+        'title' => 'Contact'
     ]);
 });
 
@@ -22,7 +27,7 @@ Route::get('/blog', function () {
     return view('blog', [
         'judul' => 'Lorem ipsum dolor sit amet.',
         'isi' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt illum voluptas iste aperiam. Ad asperiores consequuntur natus amet? Necessitatibus dolor repellat fugiat sint, ducimus temporibus corrupti soluta cumque tempore voluptas unde accusantium dolorum nesciunt eum incidunt hic? Quaerat, error expedita. Voluptas cupiditate maxime eos, rerum ipsum odit earum ipsam fuga facilis temporibus perferendis, pariatur, aspernatur libero quia! Consectetur temporibus, quasi, numquam eum voluptatum ipsam nihil esse ratione distinctio at rerum sunt similique ut repudiandae modi nostrum consequatur vel blanditiis praesentium ab debitis possimus perspiciatis enim! Explicabo, dignissimos maiores. Dignissimos fugit fugiat at ullam nam? Ut tempora laboriosam culpa sit earum.',
-
+        'title' => 'Blog'
     ]);
 });
 
